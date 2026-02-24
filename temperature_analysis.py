3b5e0784a7257a91e35fa9a32eb260c3658f6ba6
MasaiSch <vani183@gmail.com>
2026-02-24 23:43:47 +0530

    Add python loop assignment

---
+++ temperature_analysis.py
@@ -0,0 +1,44 @@
+# Name: Kalaivani
+# Roll Number: iitp_aimltn_2602650
+# Assignment: Python Loops & Automation - Subjective Question
+
+print("===== Task 1: Find Maximum and Minimum =====")
+temperatures = [28, 32, 35, 29, 31, 27, 30]
+high_temp = temperatures[0]
+low_temp = temperatures[0]
+for temp in temperatures: 
+    if(temp > high_temp):
+        high_temp = temp
+    elif(temp < low_temp):
+        low_temp = temp
+
+print(f"Highest Temperature: {high_temp}°C")
+print(f"Lowest Temperature: {low_temp}°C")
+
+
+print("\n===== Task 2: Count Hot Days =====")
+temperatures = [28, 32, 35, 29, 31, 27, 30]
+
+hot_days = 0
+for temp in temperatures:
+    if(temp <= 30):
+        continue
+    hot_days +=1
+
+print(f"Hot Days (>30°C): {hot_days}")
+
+
+print("\n===== Task 3: Alert System =====")
+temperatures = [28, 32, 35, 40, 31, 33, 30]
+
+hot_days = 0
+date = 0
+for index, temp in enumerate(temperatures,start=1):
+    if(temp == 40):
+        date = index
+        break
+    elif(temp > 30):
+        hot_days +=1
+
+print(f"Hot Days before alert: {hot_days}")
+print(f"Alert! Extreme temperature 40°C detected on Day {date}")
